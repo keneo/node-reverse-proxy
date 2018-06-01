09ff35dfb8765fc28ec9b29ad5330cd4472038d9
bartek <bartek+windows@szabat.com>
2018-06-01 11:06:59 +0200

    first commit

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM node:carbon
+#FROM ubuntu
+#RUN apt-get update
+#RUN apt-get install -y curl
+#RUN apt-get install -y gnupg
+#RUN curl -sL https://deb.nodesource.com/setup_10.x | bash -
+#RUN apt-get install -y nodejs
+
+# Create app directory
+WORKDIR /usr/src/app
+
+# Install app dependencies
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+# where available (npm@5+)
+COPY package*.json ./
+
+
+RUN npm install
+# If you are building your code for production
+# RUN npm install --only=production
+
+# Bundle app source
+COPY . .
+
+EXPOSE 8080
+
+CMD [ "npm", "start" ]
---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// Config
+const PORT = 8080;
+const HOST = '0.0.0.0';
+const TARGET_HOST = 'http://www.google-analytics.com';
+
+
+const express = require('express');
+const request = require('request');
+
+// App
+const app = express();
+
+app.use('/', function(req, res) {
+  console.log(`New request ${req.url}`);
+  var url = TARGET_HOST + req.url;
+  var r = null;
+  if(req.method === 'POST') {
+     r = request.post({uri: url, json: req.body});
+  } else {
+     r = request(url);
+  }
+
+  req.pipe(r).pipe(res);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
